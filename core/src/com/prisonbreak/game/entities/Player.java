9bf2a69b90cd1c0ba2e0abc2f176a6ecffa2e575
Krankai <bay25796@gmail.com>
2017-04-22 11:29:17 +0700

    Update tiled map. Add Player and allow user to move by using arrow keys

---
+++ core/src/com/prisonbreak/game/entities/Player.java
@@ -0,0 +1,145 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.prisonbreak.game.entities;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+/**
+ *
+ * @author krankai
+ */
+public class Player {
+    
+    private static final int FRAME_COLS = 4;
+    private static final int FRAME_ROWS = 1;
+    
+    private final TextureRegion[] playerFrames;   // 0: down/right , 1: up, 2: left, 3: sleep
+    private TextureRegion currentTexture;
+    
+    private boolean moveLeft;
+    private boolean moveRight;
+    private boolean moveUp;
+    private boolean moveDown;
+    private boolean sleep;      // for fun :)
+    
+    public float x;
+    public float y;
+    private final float velocity = 32.0f * 5;
+    
+    public Player() {
+        // load sheet image
+        Texture sheet = new Texture(Gdx.files.internal("prisoner.png"));
+        
+        // split sheet image into multiple single images
+        TextureRegion[][] tmp = TextureRegion.split(sheet, sheet.getWidth() / FRAME_COLS, sheet.getHeight() / FRAME_ROWS);
+        
+        // place all images into a 1D array
+        playerFrames = new TextureRegion[FRAME_COLS * FRAME_ROWS];
+        int index = 0;
+        for (int i = 0; i < FRAME_ROWS; ++i) {
+            for (int j = 0; j < FRAME_COLS; ++j) {
+                playerFrames[index++] = tmp[i][j];
+            }
+        }
+        
+        // initialize position
+        x = y = 0f;
+        
+        // initialize the current image
+        currentTexture = playerFrames[0];
+        
+        // initialize direction flags
+        moveLeft = moveRight = moveUp = moveDown = false;
+    }
+    
+    public TextureRegion getImage() {
+        return currentTexture;
+    }
+    
+    public void dispose() {
+       
+    }
+    
+    public void setLeftMove(boolean t) {
+        // set other direction flags to false
+        if (moveRight) moveRight = false;
+        if (moveUp) moveUp = false;
+        if (moveDown) moveDown = false;
+        
+        // set to move left or stop moving left, based on user signal
+        moveLeft = t;
+    }
+    
+    public void setRightMove(boolean t) {
+        if (moveLeft) moveLeft = false;
+        if (moveUp) moveUp = false;
+        if (moveDown) moveDown = false;
+        
+        moveRight = t;
+    }
+    
+    public void setUpMove(boolean t) {
+        if (moveLeft) moveLeft = false;
+        if (moveRight) moveRight = false;
+        if (moveDown) moveDown = false;
+        
+        moveUp = t;
+    }
+    
+    public void setDownMove(boolean t) {
+        if (moveLeft) moveLeft = false;
+        if (moveUp) moveUp = false;
+        if (moveRight) moveRight = false;
+        
+        moveDown = t;
+    }
+    
+    public void sleepWakeup() {
+        if (!sleep) {
+            if (moveLeft) moveLeft = false;
+            if (moveRight) moveRight = false;
+            if (moveUp) moveUp = false;
+            if (moveDown) moveDown = false;
+
+            sleep = true;
+            currentTexture = playerFrames[3];
+        } else {
+            sleep = false;
+            currentTexture = playerFrames[0];
+        }
+    }
+    
+    public void updateMotion() {
+        float amountX = velocity * Gdx.graphics.getDeltaTime();
+        float amountY = velocity * Gdx.graphics.getDeltaTime();
+        
+        if (moveLeft) {
+            currentTexture = playerFrames[2];
+            x -= amountX;
+        }
+        if (moveRight) {
+            currentTexture = playerFrames[0];
+            x += amountX;
+        }
+        if (moveUp) {
+            currentTexture = playerFrames[1];
+            y += amountY;
+        }
+        if (moveDown) {
+            currentTexture = playerFrames[0];
+            y -= amountY;
+        }
+    }
+    
+    public void update() {
+        if (sleep) return;
+        updateMotion();
+    }
+    
+}
--- core/src/com/prisonbreak/game/screens/GameScreen.java
+++ core/src/com/prisonbreak/game/screens/GameScreen.java
@@ -7,44 +7,53 @@ package com.prisonbreak.game.screens;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.prisonbreak.game.entities.Player;
 
 /**
  *
  * @author user
  */
-public class GameScreen implements Screen {
+public class GameScreen implements Screen, InputProcessor {
     
-    private Stage stage;
     private Game game;
+    private SpriteBatch batch;
+    private Player player;
     private OrthographicCamera camera;
     
     public GameScreen(Game aGame) {
         game = aGame;
-        stage = new Stage(new ScreenViewport());
-        camera = (OrthographicCamera) stage.getViewport().getCamera();
-        
+        batch = new SpriteBatch();
+        player = new Player();
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false);
     }
     
     @Override
     public void show() {
         Gdx.app.log("MainScreen", "show");
-        Gdx.input.setInputProcessor(stage);
+        Gdx.input.setInputProcessor(this);
     }
     
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         
         camera.update();
+        player.update();
         
-        stage.act();
-        stage.draw();
+        batch.begin();
+        batch.draw(player.getImage(), player.x, player.y);
+        batch.end();
     }
     
     @Override
@@ -69,6 +78,79 @@ public class GameScreen implements Screen {
 
     @Override
     public void dispose() {
-        stage.dispose();
+//        player.dispose();
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        switch (keycode) {
+            case Keys.LEFT:
+                player.setLeftMove(true);
+                break;
+            case Keys.RIGHT:
+                player.setRightMove(true);
+                break;
+            case Keys.UP:
+                player.setUpMove(true);
+                break;
+            case Keys.DOWN:
+                player.setDownMove(true);
+                break;
+            case Keys.SPACE:
+                player.sleepWakeup();
+                break;
+        }
+        
+        return true;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        switch (keycode) {
+            case Keys.LEFT:
+                player.setLeftMove(false);
+                break;
+            case Keys.RIGHT:
+                player.setRightMove(false);
+                break;
+            case Keys.UP:
+                player.setUpMove(false);
+                break;
+            case Keys.DOWN:
+                player.setDownMove(false);
+                break;
+        }
+        
+        return true;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(int amount) {
+        return false;
     }
 }
